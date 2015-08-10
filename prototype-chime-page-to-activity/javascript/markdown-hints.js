1da3616b7960deef5d561fca1c95239d78d96191
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2015-08-10 13:52:17 -0700

    removing second prototype

--- prototype-chime-page-to-activity/javascript/markdown-hints.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#toggle-hints').addClass('is-selected');
-
-	$('#toggle-hints').click(function() {
-		$('.previewer__hints, .previewer__content').toggleClass('is-hidden');
-		$('#toggle-hints, #toggle-preview').toggleClass('is-selected');
-	})
-})
--- prototype-chime-single-page/javascript/activity-summary.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('.activity-summary__table').addClass('is-hidden');
-
-	$('.activity-summary__toggle').click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('is-open');
-		$('.activity-summary__table').toggleClass('is-hidden');
-	});
-})
